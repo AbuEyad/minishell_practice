1de18a6e7cddcd2454cd9ba1d03c37d5fea4c2a4
AbuEyad <e0e0e2@hotmail.com>
2023-12-17 14:26:13 +0400

    17-12--2

--- main_old1.c
+++ main_old1.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_old1.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: habu-zua <habu-zua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 10:45:17 by habu-zua          #+#    #+#             */
-/*   Updated: 2023/12/17 14:19:06 by habu-zua         ###   ########.fr       */
+/*   Updated: 2023/12/17 14:18:49 by habu-zua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //***********
-// path fixed
+// path is hardcoded to /bin/ls
 //***********
-
 #include "./includes/main.h"
-
 void ft_free(char **cmd)
-
 {
 	int i;
 	i = 0;
@@ -27,29 +24,6 @@ void ft_free(char **cmd)
 		i++;
 	}
 	free(cmd);
-}
-
-char *ft_getpath(char *cmd, char *envpath)
-{
-    int i;
-    char **temp;
-    char *path;
-    char *temp2;
- 
-    temp = ft_split(envpath, ':');
-    i = 0;
-    while(temp[i])
-    {
-        temp2 = ft_strjoin(temp[i], "/");
-        path = ft_strjoin(temp2, cmd);
-        free(temp2);
-        if (access(path, F_OK) == 0)
-			return (ft_free(temp), path);
-        free(path);
-        i++;
-    }
-	ft_free(temp);
-    return (NULL);
 }
 
 int main(int ac, char **av, char **env)
@@ -67,32 +41,16 @@ int main(int ac, char **av, char **env)
 		input = readline("Prompt$ ");
 		//take care of exit
 		if (ft_strcmp(input, "exit") == 0)
-		{
-			free(input);
 			exit(0);
-		}
 	   	cmd = ft_split(input, ' ');
+		path = ft_strjoin("/bin/",cmd[0]);
 		//you can run execuve without fork but this will exit the shell once the command is done.
-		path = ft_getpath(cmd[0], getenv("PATH"));
 	   	if (fork() == 0)
-	   	{
-            execve(path, cmd, env);
-            perror("execve"); // In case execve fails
-            exit(EXIT_FAILURE); // Make sure child process exits on failure
-        }
-		else 
-			wait(NULL);
+	   		execve(path, cmd, env);
+		wait(NULL); //wait for child to finish.
 		free(path);
 		ft_free(cmd);
 	}
+   
 	return (0);
 }
-
-
-
-/*
-	this part to test leaks if file not closed.
-	int fd = open("test.txt", O_RDWR | O_CREAT | O_APPEND, 0644);
-	close(fd);
-
-*/
